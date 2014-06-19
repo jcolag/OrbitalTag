dd273a2411db81fd77693d0260a8e99ce9d8ba65
John Colagioia <john@colagioia.net>
2014-06-19 18:32:46 -0400

    Add dots to avoid

--- tag.js
+++ tag.js
@@ -1,7 +1,8 @@
+var svgNS = "http://www.w3.org/2000/svg";
+var xlinkNS = "http://www.w3.org/1999/xlink";
 window.addEventListener("keypress", keypressHandler, false);
 
 function keypressHandler(e) {
-placeDot(10, 10);
   var svg = document.getElementById("game");
   var svgDoc = svg.contentDocument;
   var hand = svgDoc.getElementById("hand");
@@ -35,15 +36,27 @@ placeDot(10, 10);
   rotate.setAttributeNS(null, "dur", dur + 's');
 }
 
-function placeDot(distance, theta) {
-  var NS = "http://www.w3.org/2000/svg";
+function placeDot(count) {
   var svg = document.getElementById("game");
   var svgDoc = svg.contentDocument;
-  var dot = svgDoc.createElementNS(NS, "circle");
-  dot.setAttributeNS(null, "id", "dot0");
-  dot.setAttributeNS(null, "cx", 20);
-  dot.setAttributeNS(null, "cy", 20);
-  dot.setAttributeNS(null, "r", 5);
-  dot.setAttributeNS(null, "fill", "red");
+  var g = svgDoc.getElementById("group");
+  var back = svgDoc.getElementById("back");
+  var radius = parseInt(back.getAttribute("r"));
+
+  for (var i = 0; i < count; i++) {
+    var r = Math.floor(Math.random() * radius);
+    var theta = Math.floor(Math.random() * 360);
+    var x = Math.floor(Math.sin(theta) * r);
+    var y = Math.floor(Math.cos(theta) * r);
+
+    var dot = document.createElementNS(svgNS, "circle");
+    dot.setAttributeNS(null, "id", "dot" + i);
+    dot.setAttributeNS(null, "cx", x + 200);
+    dot.setAttributeNS(null, "cy", y + 200);
+    dot.setAttributeNS(null, "r", 5);
+    dot.setAttributeNS(null, "fill", "orange");
+
+    g.appendChild(dot);
+  }
 }
 
